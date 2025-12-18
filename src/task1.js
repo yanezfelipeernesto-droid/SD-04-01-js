15dc9fbde99809ab4f8a1084fca721f7b07c683b
Felipe Ernesto Yañez-Sadoval <yanezfelipeernesto@gmail.com>
2025-12-17 22:54:14 -0600

    task1.js commited

--- src/task1.js
+++ src/task1.js
@@ -1,7 +1,14 @@
-const arr = []
+let arr = []
 
 // Escribe tu codigo por debajo de esta linea
 
+
+
+for (let i = 1; i<=20; i++){
+arr.push(i)
+};
+
+//console.log(array1); 
 
 
 // Escribe tu codigo por encima de esta linea
